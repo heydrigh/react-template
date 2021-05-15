7f0335c152991f38abf13a474aba75881902b5a6
heydrigh <heydrigh@gmail.com>
2021-05-15 12:05:31 -0300

    refactor: adjust to use theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import GlobalStyles from "./styles/global";
+import React from 'react';
+import GlobalStyles from './styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 const App = () => {
-	return (
-		<div>
-			<h1>Hello World</h1>
-			<GlobalStyles />
-		</div>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <h1>Hello World</h1>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    success: '#1AC88D',
+    fail: '#FF4040',
+    warn: '#F2CF01',
+    grey: '#6B6B6B',
+    darkGrey: '#8A8A8A',
+    lightGrey: '#c9c9c9',
+    badgeGrey: '#e6eaf1'
+  },
+  fontSizes: {
+    default: '1.4rem',
+    small: '1.2rem',
+    mid: '1.6rem',
+    large: '2.0rem',
+    xlarge: '2.5rem'
+  }
+};
+
+export default theme;
